730969451e286ea04ea68dc9eefb6122966c4362
nghiamt <mtn_khtn@yahoo.com>
2012-10-28 13:33:19 +0900

    add file

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Testamazon::Application.config.secret_token = 'c1cc9ae2b90b3bc2132cdaade30801b033ecc6bdcf68c4fc231704433b752e740baf0df4beb5906d40ca36b6c39371fbc6de355fa8cea12949ce85c03cb4b488'
